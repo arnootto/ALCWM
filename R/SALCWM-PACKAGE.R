81b6b87c6c4d7a2981cf69fbd884a99bc402ab8c
arnootto <61681469+arnootto@users.noreply.github.com>
2025-05-07 16:31:40 +0200

    update

---
+++ R/SALCWM-PACKAGE.R
@@ -0,0 +1,29 @@
+#' Mixture of multivariate linear asymmetric Laplace regressions with multiple asymmetric Laplace covariates
+#'
+#' This package fits the shifted asymmetric Laplace cluster-weighted model and the contaminated shifted asymmetric Laplace cluster-weighted model via an EM-algorithm, as described in "Mixture of multivariate linear asymmetric Laplace regressions with multiple asymmetric Laplace covariates" (2025).
+#'
+#'@section Key Functions:
+#' \itemize{
+#'   \item \code{\link{SALCWM}}: Fits the Shifted Asymmetric Laplace Cluster-Weighted Model.
+#'   \item \code{\link{CSALCWM}}: Fits the Contaminated Shifted Asymmetric Laplace Cluster-Weighted Model.
+#'   \item \code{\link{dSAL}}: Evaluates the density of the Shifted Asymmetric Laplace distribution.
+#'   \item \code{\link{dCSAL}}: Evaluates the density of the Contaminated Shifted Asymmetric Laplace distribution.
+#' }
+#'
+#' @section Getting Started:
+#' To get started, try the examples in \code{\link{my_exported_function}}.
+#'
+#'@examples
+#'library(sn)
+#'data("ais")
+#'data=ais
+#'subset=data[,c("sex","RCC","WCC","BMI","SSF","Bfat","LBM")]
+#'est1=SALCWM(Y=cbind(data$RCC,data$WCC),X=cbind(data$BMI,data$SSF,data$Bfat,data$LBM),G=2,tol=1e-5, max.it=2000,initialization = "mclust")
+#'est2=CSALCWM(Y=cbind(data$RCC,data$WCC),X=cbind(data$BMI,data$SSF,data$Bfat,data$LBM),G=2, tol=1e-5,max.it=2000,initialization = "mclust")
+#'
+#' @docType package
+#' @name SALCWM
+#' @aliases SALCWM-package
+#' @author Arno Otto <arno.otto@up.ac.za>, Andriëtte  Bekker, Antonio Punzo, Johan Ferreira, Cristina Tortora.
+#' @keywords package
+NULL
